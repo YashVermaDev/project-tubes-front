9df5d27b41339aad4ec2f4815dc536656bf29cc3
SurajGoswami98 <suraj98kumar.sg@gmail.com>
2024-09-14 07:17:36 +0530

    ui updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const button = ({ type, value, classes = '' }) => {
     return (
-        <button type={type} className={"px-2 py-3 border-2 rounded-sm bg-blue-500 min-w-[100px] uppercase text-xs font-bold hover:bg-black" + classes}>{value}</button>
+        <button type={type} className={"px-2 py-3 border rounded-sm bg-blue-500 min-w-[100px] uppercase text-xs font-bold hover:bg-black" + classes}>{value}</button>
     )
 }
 
